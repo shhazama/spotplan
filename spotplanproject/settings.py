743d079ff8b209b9db9bdac06683e42b84623bff
shhazama <sh-hazama@outlook.jp>
2022-11-23 21:08:49 +0900

    ok

--- spotplanproject/settings.py
+++ spotplanproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=f69!et(bhvzlbw^yldcsmf4nu!j8l)-rfl6o&11i%hvfpag=j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
